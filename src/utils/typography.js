b34d9804ba99c534f480d997450a9302f9c4b847
Rahul Rangnekar <rrangnekar96@gmail.com>
2018-05-05 11:00:43 -0700

    add typography

---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from 'typography';
+import noriegaTheme from 'typography-theme-noriega';
+
+const typography = new Typography(noriegaTheme);
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles();
+}
+
+export default typography;
