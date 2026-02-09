4c84d8d36831b34a68b4d155d388c79ba495f91c
Rahul Rangnekar <rahrang.dev@gmail.com>
2026-02-08 17:12:37 -0800

    move the cookie project into website, add index page for projects

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 export const homeRoute = () => '/';
 
+export const projectsRoute = () => `/projects`;
+
 export const blogRoute = () => `/blog`;
 
 export const postRoute = (slug: string) => `${blogRoute()}/${slug}`;
